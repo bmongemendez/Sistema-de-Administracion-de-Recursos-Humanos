fb3c676433fb84bf09a4cdf98966479cdfe439df
Brayan Monge <bmongemendez@gmail.com>
2021-02-22 23:35:38 -0600

    first commit

---
+++ SARH - JMéndez Constructora/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SARH___JMéndez_Constructora.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
