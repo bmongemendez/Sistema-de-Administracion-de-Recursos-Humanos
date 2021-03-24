d49333c06693e80bd836a002366bfdc20d736843
ByronLeal <dev7CE@hotmail.com>
2021-03-23 19:06:30 -0600

    Refactor: Theme Swicth issue

--- SARH - JMéndez Constructora/wwwroot/bundles/mainscripts.bundle.js
+++ SARH - JMéndez Constructora/wwwroot/bundles/mainscripts.bundle.js
@@ -179,8 +179,8 @@ $(function () {
 			}),
 			$('.setting_switch .lv-btn').on('change', function () {
 				this.checked
-					? $('body').addClass('light_version')
-					: $('body').removeClass('light_version');
+					? $('body').removeClass('light_version')
+					: $('body').addClass('light_version');
 			}),
 			$('.setting_switch .mini-sidebar-btn').on('change', function () {
 				this.checked
@@ -205,68 +205,68 @@ $(function () {
 			$('.setting_switch .rtl-btn').on('change', function () {
 				this.checked ? $('body').addClass('rtl') : $('body').removeClass('rtl');
 			});
-	}),
-	($.fn.clickToggle = function (t, n) {
-		return this.each(function () {
-			var e = !1;
-			$(this).bind('click', function () {
-				return e
-					? ((e = !1), n.apply(this, arguments))
-					: ((e = !0), t.apply(this, arguments));
-			});
-		});
-	}),
-	particlesJS('particles-js', {
-		particles: {
-			number: { value: 30, density: { enable: !0, value_area: 700 } },
-			color: { value: ['#fc3c5f', '#993cfc', '#3ca9fc', '#3cfc5f', '#fcdf3c'] },
-			shape: {
-				type: 'circle',
-				stroke: { width: 0, color: '#000000' },
-				polygon: { nb_sides: 15 },
-			},
-			opacity: {
-				value: 0.5,
-				random: !1,
-				anim: { enable: !1, speed: 1.2, opacity_min: 0.15, sync: !1 },
-			},
-			size: {
-				value: 2.5,
-				random: !1,
-				anim: { enable: !0, speed: 2, size_min: 0.15, sync: !1 },
-			},
-			line_linked: {
-				enable: !0,
-				distance: 110,
-				color: '#2b313c',
-				opacity: 1,
-				width: 1,
-			},
-			move: {
-				enable: !0,
-				speed: 1.6,
-				direction: 'none',
-				random: !1,
-				straight: !1,
-				out_mode: 'out',
-				bounce: !1,
-				attract: { enable: !1, rotateX: 600, rotateY: 1200 },
-			},
-		},
-		interactivity: {
-			detect_on: 'canvas',
-			events: {
-				onhover: { enable: !1, mode: 'repulse' },
-				onclick: { enable: !1, mode: 'push' },
-				resize: !0,
-			},
-			modes: {
-				grab: { distance: 400, line_linked: { opacity: 1 } },
-				bubble: { distance: 400, size: 30, duration: 2, opacity: 8, speed: 3 },
-				repulse: { distance: 200, duration: 0.4 },
-				push: { particles_nb: 4 },
-				remove: { particles_nb: 2 },
-			},
-		},
-		retina_detect: !0,
-	});
+	});//,
+	//($.fn.clickToggle = function (t, n) {
+	//	return this.each(function () {
+	//		var e = !1;
+	//		$(this).bind('click', function () {
+	//			return e
+	//				? ((e = !1), n.apply(this, arguments))
+	//				: ((e = !0), t.apply(this, arguments));
+	//		});
+	//	});
+	//}),
+	//particlesJS('particles-js', {
+	//	particles: {
+	//		number: { value: 30, density: { enable: !0, value_area: 700 } },
+	//		color: { value: ['#fc3c5f', '#993cfc', '#3ca9fc', '#3cfc5f', '#fcdf3c'] },
+	//		shape: {
+	//			type: 'circle',
+	//			stroke: { width: 0, color: '#000000' },
+	//			polygon: { nb_sides: 15 },
+	//		},
+	//		opacity: {
+	//			value: 0.5,
+	//			random: !1,
+	//			anim: { enable: !1, speed: 1.2, opacity_min: 0.15, sync: !1 },
+	//		},
+	//		size: {
+	//			value: 2.5,
+	//			random: !1,
+	//			anim: { enable: !0, speed: 2, size_min: 0.15, sync: !1 },
+	//		},
+	//		line_linked: {
+	//			enable: !0,
+	//			distance: 110,
+	//			color: '#2b313c',
+	//			opacity: 1,
+	//			width: 1,
+	//		},
+	//		move: {
+	//			enable: !0,
+	//			speed: 1.6,
+	//			direction: 'none',
+	//			random: !1,
+	//			straight: !1,
+	//			out_mode: 'out',
+	//			bounce: !1,
+	//			attract: { enable: !1, rotateX: 600, rotateY: 1200 },
+	//		},
+	//	},
+	//	interactivity: {
+	//		detect_on: 'canvas',
+	//		events: {
+	//			onhover: { enable: !1, mode: 'repulse' },
+	//			onclick: { enable: !1, mode: 'push' },
+	//			resize: !0,
+	//		},
+	//		modes: {
+	//			grab: { distance: 400, line_linked: { opacity: 1 } },
+	//			bubble: { distance: 400, size: 30, duration: 2, opacity: 8, speed: 3 },
+	//			repulse: { distance: 200, duration: 0.4 },
+	//			push: { particles_nb: 4 },
+	//			remove: { particles_nb: 2 },
+	//		},
+	//	},
+	//	retina_detect: !0,
+	//});
