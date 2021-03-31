70bb39f4f59f29a1084fc12802e24e57df971096
ByronLeal <dev7CE@hotmail.com>
2021-03-31 03:10:26 -0600

    Feat: AuditRecent Events in Dashboard

--- SARH - JMéndez Constructora/Controllers/TableroController.cs
+++ SARH - JMéndez Constructora/Controllers/TableroController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SARH___JMéndez_Constructora.Data;
 using SARH___JMéndez_Constructora.Models;
+using SARH___JMéndez_Constructora.Models.TableroViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,14 +14,18 @@ namespace SARH___JMéndez_Constructora.Controllers
     public class TableroController : Controller
     {
         private readonly ILogger<TableroController> _logger;
+        private readonly ApplicationDbContext _appContext;
 
-        public TableroController(ILogger<TableroController> logger)
+        public TableroController(ILogger<TableroController> logger, ApplicationDbContext appContext)
         {
             _logger = logger;
+            _appContext = appContext;
         }
 
         public IActionResult Index()
         {
+            ViewData["AuditRecent"] = BindAuditRecentViewModel 
+            (GetBitacoraEmpleados(), GetBitacoraPagos());
             return View();
         }
 
@@ -28,5 +34,54 @@ namespace SARH___JMéndez_Constructora.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #region Helpers
+        private IEnumerable<Empleadosregistroauditoria> GetBitacoraEmpleados ()
+        {
+            return _appContext.Empleadosregistroauditoria.ToList()
+                .OrderByDescending(p => p.FechaHoraTransaccion)
+                .Take(10);;
+        }
+        private IEnumerable<Pagosregistroauditoria> GetBitacoraPagos ()
+        {
+            return _appContext.Pagosregistroauditoria.ToList()
+                .OrderByDescending(p => p.FechaHoraTransaccion)
+                .Take(10);
+        }
+        private IEnumerable<AuditRecentViewModel> BindAuditRecentViewModel (
+            IEnumerable<Empleadosregistroauditoria> empleadosAuditList, 
+            IEnumerable<Pagosregistroauditoria> pagosAuditList)
+        {
+            List<AuditRecentViewModel> list = new List<AuditRecentViewModel>();
+            foreach (var empleado in empleadosAuditList)
+            {
+                list.Add(new AuditRecentViewModel
+                {
+                    Tabla = "Empleados",
+                    IdTabla = empleado.IdEmpleado,
+                    Columna = empleado.Columna,
+                    FechaHoraTransaccion = empleado.FechaHoraTransaccion,
+                    ValorAnterior = empleado.ValorAnterior,
+                    ValorNuevo = empleado.ValorNuevo,
+                    UserName = empleado.UserName
+                });
+            } 
+            foreach (var pago in pagosAuditList)
+            {
+                list.Add (new AuditRecentViewModel
+                {
+                    Tabla = "Pagos",
+                    IdTabla = pago.IdPago,
+                    Columna = pago.Columna,
+                    FechaHoraTransaccion = pago.FechaHoraTransaccion,
+                    ValorAnterior = pago.ValorAnterior,
+                    ValorNuevo = pago.ValorNuevo,
+                    UserName = pago.UserName
+                });
+            }
+            return list
+                .OrderByDescending(ar => ar.FechaHoraTransaccion)
+                .Take(10);
+        }
+        #endregion
     }
 }
